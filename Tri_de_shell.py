0f53e132fbcd76dfb1a805694557f5523c47ebb9
Gitravail <raphael.tournafond@gmail.com>
2018-10-19 14:28:42 +0200

    fin séance

--- Tri_de_shell.py
+++ Tri_de_shell.py
@@ -1,13 +1,12 @@
-N = 10
-
 def Tri_shell(t):
-    incr = N//2
+    n = len(t)
+    incr = n//2
     while True:
 
-        for k in range (1, incr):
+        for k in range (1, incr + 1):
             # tri la k-ieme série
 
-            for i in range (incr + k, N, incr):
+            for i in range (incr + k, n, incr):
                 # insertion de t[i] dans sa série
                 x = t[i]
                 j = i - incr
@@ -22,7 +21,7 @@ def Tri_shell(t):
         if incr == 1:
             break
 
-t = [5, 41, 53, 11, 20, 5, 1, 9, 12, 102, 15]
+t = [5, 41, 53, 11, 20, 5, 1, 9, 12, 102]
 print("Avant : ", t)
 
 Tri_shell(t)
